c6fa65d5e3c26bbbfc6c7d40a32de30dce43b836
KombinatorVit <slonik0775@gmail.com>
2023-02-26 00:47:33 +0200

    строю структуру проекта и зависимости

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,23 @@
+import {FC} from 'react';
+import {Link} from 'react-router-dom';
+
+
+export const Header: FC = () => {
+
+
+    return (
+        <header>
+            <nav className="px-2 py-4">
+
+                <div className="flex justify-between items-center">
+                    <Link
+                        to="/"
+                        className="font-titillilum text-2xl mr-8 text-conduit-green"
+                    >
+                        conduit
+                    </Link>
+                </div>
+            </nav>
+        </header>
+    );
+};
